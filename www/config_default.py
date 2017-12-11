7d2b01faee56d261ce717c53dabcb003644d55cd
jack <defendingthebottom@foxmail.com>
2017-12-11 14:40:40 +0800

    完成读取config

---
+++ www/config_default.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Q1mi"
+# Date: 2017/12/11
+
+'''
+Default configurations
+'''
+
+configs = {
+    'debug':True,
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'www',
+        'password':'www',
+        'db':'awesome'
+    },
+    'session':{
+        'secret':'Awesome'
+    }
+}
+
